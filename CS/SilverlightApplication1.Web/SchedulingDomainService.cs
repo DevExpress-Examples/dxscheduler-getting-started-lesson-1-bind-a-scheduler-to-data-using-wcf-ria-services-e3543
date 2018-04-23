532fc270a1001a9ebf0abc2288c924e1cd00ed22
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication1.Web/SchedulingDomainService.cs
@@ -0,0 +1,53 @@
+﻿
+namespace SilverlightApplication1.Web {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // Implements application logic using the CarsDBEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public class SchedulingDomainService : LinqToEntitiesDomainService<CarsDBEntities> {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'CarScheduling' query.
+        public IQueryable<CarScheduling> GetCarScheduling() {
+            return this.ObjectContext.CarScheduling;
+        }
+
+        public void InsertCarScheduling(CarScheduling carScheduling) {
+            if ((carScheduling.EntityState != EntityState.Detached)) {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(carScheduling, EntityState.Added);
+            } else {
+                this.ObjectContext.CarScheduling.AddObject(carScheduling);
+            }
+        }
+
+        public void UpdateCarScheduling(CarScheduling currentCarScheduling) {
+            this.ObjectContext.CarScheduling.AttachAsModified(currentCarScheduling, this.ChangeSet.GetOriginal(currentCarScheduling));
+        }
+
+        public void DeleteCarScheduling(CarScheduling carScheduling) {
+            if ((carScheduling.EntityState != EntityState.Detached)) {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(carScheduling, EntityState.Deleted);
+            } else {
+                this.ObjectContext.CarScheduling.Attach(carScheduling);
+                this.ObjectContext.CarScheduling.DeleteObject(carScheduling);
+            }
+        }
+    }
+}
+
+
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# DXScheduler Getting Started - Lesson 1 - Bind a Scheduler to Data Using WCF RIA Services
+
+
+<p>This example demonstrates how to create a simple scheduling Silverlight application with <strong>SchedulerControl</strong> bound to data.  It can be useful for you in solving this task if you are a first-time user of the DXScheduler Suite for Silverlight.</p>
+
+
+<h3>Description</h3>
+
+<p>You can replicate the same project by performing steps from the following tutorial: <a href="http://documentation.devexpress.com/#Silverlight/CustomDocument5728"><u>DXScheduler - Bind a Scheduler to Data Using WCF RIA Services</u></a>.</p>
+<p>This example requires the availability of a local SQL Server 2008 R2 Express instance (&ldquo;.\SQLEXPRESS&rdquo;). If you have another version of SQL Server installed on your computer, set <strong>data</strong> <strong>source</strong> in the connection string (the Web.config file) to the corresponding SQL Server instance name.</p>
+<p>All Getting Started Examples: <strong>Lesson 1</strong> <a href="http://www.devexpress.com/Support/Center/p/E3547.aspx"><u>Lesson 2</u></a> <a href="http://www.devexpress.com/Support/Center/p/E3603.aspx"><u>Lesson 3</u></a> <a href="http://www.devexpress.com/Support/Center/p/E3548.aspx"><u>Lesson 4</u></a> <a href="http://www.devexpress.com/Support/Center/p/E3598.aspx"><u>Lesson 5</u></a></p>
+
+<br/>
+
+
